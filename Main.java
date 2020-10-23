21d1193454131d8d011c239e6954780db443e2a2
AkMo3 <akmo3901@gmail.com>
2020-10-24 01:40:15 +0530

    fix: birthdate formatting

--- Main.java
+++ Main.java
@@ -2,88 +2,146 @@ package passwordGenerator;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-
 public class Main {
+	static Scanner sc = new Scanner(System.in);
+	String Filename_Global = "";
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
-			
-			@SuppressWarnings("resource")
-			Scanner sc = new Scanner(System.in);
 			ArrayList<String> Parameters = new ArrayList<String>();
 			ArrayList<String> Parameter_Value = new ArrayList<String>();
 			ArrayList<String> Integer_Value = new ArrayList<String>();
-			Parameters.add("Name");
-			System.out.println(Parameters.get(0));
-			Parameter_Value.add(sc.nextLine());
-			Parameters.add("Birthdate");
-			System.out.println("Birthdate in DDMMYYYY");
-			Parameter_Value.add(sc.nextLine());
+//			HashMap<String, Integer> Password = new HashMap<>();
+//			int Password_Count =0;
+
+			AddParameter(Parameters, Parameter_Value, "Name");
+			AddParameter(Parameters, Parameter_Value, "Birthdate(DDMMYYYY)");
+
 			Integer_Value.add(Parameter_Value.get(1));
-			Parameters.add("Father_Name");
-			System.out.println("Father's Name");
-			Parameter_Value.add(sc.nextLine());
-			Parameters.add("Mother_Name");
-			System.out.println("Mothers's Name");
-			Parameter_Value.add(sc.nextLine());
-			Parameters.add("Person1_Name");
-			System.out.println("Other important name");
-			Parameter_Value.add(sc.nextLine());
-			char[] Special_Char = {'@', '$', '.', '/', ' ',',', '&'};
-			String file = "F:\\PasswordGenerator\\"+Parameter_Value.get(0)+".txt";
+
+			AddParameter(Parameters, Parameter_Value, "Father's Name");
+			AddParameter(Parameters, Parameter_Value, "Mother's Name");
+			AddParameter(Parameters, Parameter_Value, "Important Person");
+
+			char[] Special_Char = { '@', '$', '.', '/', ' ', ',', '&' };
+
+			String file = "F:\\PasswordGenerator\\" + Parameter_Value.get(0) + ".txt";
+
+			FormatBirthdate(Integer_Value);
+
 			FileWriter fw = new FileWriter(file);
-			boolean run = true;
-			while(run) { 
-				String Birthdate = Parameter_Value.get(1);
-				for(int i =0;i<Birthdate.length();i++) {
-					for(int j =i; j<=Birthdate.length();j++) {
-						if(j>=i) { 	
-							String password = Parameter_Value.get(0) + Birthdate.substring(i,j);
+
+			for (int k = 0; k < Integer_Value.size(); k++) {
+				String Birthdate = Integer_Value.get(k);
+				for (int i = 0; i < Birthdate.length(); i++) {
+					for (int j = i; j <= Birthdate.length(); j++) {
+						if (j >= i) {
+							String password = Parameter_Value.get(0) + Birthdate.substring(i, j);
 							fw.write(password);
-							fw.write("\n");
-							for(int len =0; len<Parameter_Value.size();len++) {
+							fw.write("\n");	
+							for (int len = 0; len < Parameter_Value.size(); len++) {
 								String name = Parameter_Value.get(len);
-								for(int s =0;s<Special_Char.length;s++) {
-									password = name + Special_Char[s] + Birthdate.substring(i,j);
+								for (int s = 0; s < Special_Char.length; s++) {
+									password = name + Special_Char[s] + Birthdate.substring(i, j);
 									fw.write(password);
-									fw.write("\n");
-									if(name!=name.toLowerCase()) 
-										{password = name.toLowerCase() + Special_Char[s] + Birthdate.substring(i,j);
+									fw.write("\n");	
+
+									if (name != name.toLowerCase()) {
+										password = name.toLowerCase() + Special_Char[s] + Birthdate.substring(i, j);
 										fw.write(password);
-										fw.write("\n");}
-									if(name!=name.toLowerCase())
-										{password = name.toUpperCase() + Special_Char[s] + Birthdate.substring(i,j);
+										fw.write("\n");	
+									}
+
+									if (name != name.toLowerCase()) {
+										password = name.toUpperCase() + Special_Char[s] + Birthdate.substring(i, j);
 										fw.write(password);
-										fw.write("\n");}
+										fw.write("\n");	
+									}
 								}
-								for(int s1 =0;s1<Special_Char.length;s1++) {
-									for(int s2 =0;s2<Special_Char.length;s2++)
-										password = name + Special_Char[s1] + Birthdate.substring(i,j)+ Special_Char[s1];
+
+								for (int s1 = 0; s1 < Special_Char.length; s1++) {
+									for (int s2 = 0; s2 < Special_Char.length; s2++)
+										password = name + Special_Char[s1] + Birthdate.substring(i, j)
+												+ Special_Char[s1];
 										fw.write(password);
-										fw.write("\n");
-										if(name!=name.toLowerCase())
-											{password = name.toLowerCase() + Special_Char[s1] + Birthdate.substring(i,j)+ Special_Char[s1];
-											fw.write(password);
-											fw.write("\n");}
-										if(name!=name.toUpperCase())
-											{password = name.toUpperCase() + Special_Char[s1] + Birthdate.substring(i,j)+ Special_Char[s1];
-											fw.write(password);
-											fw.write("\n");}
+										fw.write("\n");	
+
+									if (name != name.toLowerCase()) {
+										password = name.toLowerCase() + Special_Char[s1] + Birthdate.substring(i, j)
+												+ Special_Char[s1];
+										fw.write(password);
+										fw.write("\n");	
+									}
+
+									if (name != name.toUpperCase()) {
+										password = name.toUpperCase() + Special_Char[s1] + Birthdate.substring(i, j)
+												+ Special_Char[s1];
+										fw.write(password);
+										fw.write("\n");	
+									}
 								}
 							}
 						}
-						if(i==Birthdate.length()-1) run=false;
-						}
 					}
 				}
-				
-				fw.close();
-		}catch(Exception e) {System.out.println(e);}
+			}
+
+			fw.close();
+
+			sc.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		
+		
+
 		System.out.println("Written");
 	}
 
+	private static void AddParameter(ArrayList<String> Parameter, ArrayList<String> Parameter_Value,
+			String ParameterName) {
+		Parameter.add(ParameterName);
+		System.out.println(ParameterName);
+		Parameter_Value.add(sc.nextLine());
+	}
+
+	private static void FormatBirthdate(ArrayList<String> Birthdate) {
+
+		String Orignal_Birthdate = Birthdate.get(0);
+
+		String Formated_birthdate = Orignal_Birthdate.substring(0, 4) + Orignal_Birthdate.substring(6, 8);
+		Birthdate.add(Formated_birthdate);
+
+		if (Orignal_Birthdate.charAt(0) == '0') {
+			String Formated_DOB = Orignal_Birthdate.substring(1);
+			Birthdate.add(Formated_DOB);
+		}
+
+		if (Orignal_Birthdate.charAt(2) == '0') {
+			String Formated_DOB = Orignal_Birthdate.substring(1);
+			Birthdate.add(Formated_DOB);
+		}
+
+		if (Orignal_Birthdate.charAt(2) == '0' && Orignal_Birthdate.charAt(0) == '0') {
+			String Formated_DOB = Orignal_Birthdate.substring(1, 2) + Orignal_Birthdate.substring(3, 4)
+					+ Orignal_Birthdate.substring(6, 8);
+			Birthdate.add(Formated_DOB);
+			Formated_DOB = Orignal_Birthdate.substring(1, 2) + Orignal_Birthdate.substring(3, 4)
+					+ Orignal_Birthdate.substring(4, 8);
+			Birthdate.add(Formated_DOB);
+		}
+	}
+	
+//	private static void AddPassword(HashMap<String, Integer> Password, String password ,int Password_Count, String file) {
+//		Password.putIfAbsent(password, Password_Count);
+//		if(Password.size() != Password_Count) {
+////				fw.write(password);
+////				fw.write("\n");	
+//				Password_Count++;
+//		} 
+//}
 }
